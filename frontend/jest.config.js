1b9bfff219488528abc8da318d4e16c6f4064294
Karsten Bak Malle <Karsten_malle@hotmail.com>
2024-11-03 20:12:30 +0100

    frontend test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest', // Use babel-jest for TypeScript and JSX files
-  },
+  preset: 'ts-jest', // Use ts-jest to handle TypeScript and JSX
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1', // Map @/ to the src directory
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock out CSS imports
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Use ts-jest to handle TypeScript files
   },
 }
